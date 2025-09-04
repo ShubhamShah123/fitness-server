66ea92010164248abcc0269931f3548de919e828
Shubham Shah <shahshubham845@gmail.com>
2025-09-04 16:40:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime
 from collections import defaultdict
+from meals import MealsData
 import os
 import pyrebase
 import hashlib
 import pandas as pd
 import numpy as np
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -113,6 +115,97 @@ def index():
 	print("Hello World")
 	return jsonify({'date': datetime.now(), 'msg': 'hello world'}), 200
 
+# Firestore data adding.
+@app.route('/add_data')
+def add_data():
+	data = pd.read_csv('./datasets/schedule.csv')
+	print(data)
+	print("Dict:")
+	data['day'] = data['day'].str.lower()
+	to_add_data = data.set_index('day').to_dict(orient='index')
+	for day in to_add_data:
+		to_add_data[day]['exercise_key'] = ''
+	res = db.child('dataset').child('schedule').set(to_add_data)
+	if res:
+		return jsonify({'status':'data added'}), 200
+	else:
+		return jsonify({'status': 'Some error happened pushing data.'}), 400
+
+@app.route('/add_exercise')
+def add_exercise():
+	print("---- adding exercise ----")
+	# days = ['monday','tuesday','wednesday','thursday','friday','saturday']
+	days = ['wednesday']
+	for day in days:
+		df = pd.read_csv(f'./datasets/{day}.csv', dtype={'id': str})
+		print(f"[+] Reading {day}.csv ...")
+		df['id'] = df['id'].astype(str)
+		to_add_data = df.set_index('id').to_dict(orient='index')
+		print(to_add_data)
+
+		# Replace NaN with None and make sure everything is JSON serializable
+		for outer_k, inner_dict in to_add_data.items():
+			for inner_k, v in inner_dict.items():
+				if pd.isna(v):  # catches np.nan, None, NaT
+					inner_dict[inner_k] = None
+		json.dumps(to_add_data)
+		res = db.child('dataset').child('exercise').push(to_add_data)
+		sch = db.child('dataset').child('schedule').child(day).update({'exercise_key': res['name']})
+	return jsonify({"status": "data added and key updated."}), 200
+
+@app.route('/add_meals')
+def add_meals():
+	print("--- adding meals ---")
+	
+	for meal in MealsData:
+		meal_id = meal['id']
+
+		# First store the main meal data (day & type)
+		db.child('dataset').child('meals').child(str(meal_id)).set({
+			'day': meal['day'],
+			'type': meal['type']
+		})
+
+		# Then store each detail item as its own node
+		for detail in meal['details']:
+			meal_num = detail['meal_num']
+
+			# Store detail under "details/{meal_num}"
+			db.child('dataset').child('meals').child(str(meal_id)).child('details').child(meal_num).set({
+				'meal_type': detail['meal_type'],
+				'meal_name': detail['meal_name'],
+				'recipe': detail['recipe']
+			})
+
+	return jsonify({'status': 'Meals and details added to DB'}), 200
+
+@app.route('/get_meals_schedule', methods=['GET'])
+def get_meals_schedule():
+	print("--- get meals schedule ---")
+	meals = db.child('dataset').child('meals').get().val()
+	# Remove None from details lists
+	to_send_list = []
+	for meal_id, meal in enumerate(meals[1:]):
+		meal['details'][:] = [d for d in meal['details'] if d is not None]
+		to_send_dict = {
+			'id':meal_id+1,
+			'day': meal['day'],
+			'type': meal['type'],
+			'num_meals': len(meal['details'])
+		}
+		to_send_list.append(to_send_dict)
+	return jsonify({'status': 'Successfull!','data': to_send_list}), 200
+
+@app.route('/get_meal_data/<id>', methods=['GET'])
+def get_meal_data(id):
+	print(f"--- Getting Meals data for ID: {id} | {type(id)}---")
+	meal_data = db.child('dataset').child('meals').child(id).get().val()
+	print(meal_data)
+	meal_data['details'][:] = [d for d in meal_data['details'] if d is not None]
+	print(meal_data)
+	return jsonify({'status':'Got the data!','data':meal_data}), 200
+#----------------------------------------------------------
+
 @app.route('/signup', methods=['POST'])
 def signup():
 	data = request.get_json()
@@ -168,14 +261,7 @@ def login():
 		if email == user.val()['email']:
 			if hashed_password == user.val()['password']:
 				flag = 1 if user.val()['role'] == 'admin' else 0
-				to_send_data = {
-					'firstName': user.val()['firstName'],
-					'lastName': user.val()['lastName'],
-					'email': user.val()['email'],
-					'phoneNumber': user.val()['phoneNumber']
-				}
-				to_send_list.append(to_send_data)
-				return jsonify({'msg': 'Succesfully Logged in !', 'key': key, 'status_code': 200, 'flag': flag, 'data': to_send_list}), 200
+				return jsonify({'msg': 'Succesfully Logged in !', 'key': key, 'status_code': 200, 'flag': flag}), 200
 			else:
 				return jsonify({'msg': 'Password Incorrect', 'key': None, 'status_code': 401}), 401
 		
@@ -184,61 +270,58 @@ def login():
 @app.route('/get_workout_schedule_v2', methods=['GET'])
 def get_workout_schedule_v2():
 	print("# ---- get_workout_schedule_v2 ----")
-	schedDf = pd.read_csv('./datasets/schedule.csv')
-	to_send_list = schedDf.to_dict(orient="records")
-	return jsonify({'data': to_send_list, 'status_code': 200}), 200	
+	sched = db.child('dataset').child('schedule').get()
+	vals = sched.val()
 
-@app.route('/get_workout_details_v2', methods=['POST'])
-def get_workout_details_v2():
-	print("# ---- get_workout_details_v2 ----")
-	day = request.get_json()['day'].lower()
-	if day == 'sunday':
-		return jsonify({'data': [1, "REST DAY", '', ''], 'status_code': 200}), 200	
-	try:
-		dayDf = pd.read_csv(f'./datasets/{day}.csv')
-	except FileNotFoundError as e:
-		return jsonify({'error': str(e), 'status_code': 404}), 404
-	# end try
-	dayDf['desc'].replace(np.nan, '', inplace=True)
-	to_send_list = dayDf.to_dict(orient="records")
-	return jsonify({'data': to_send_list, 'status_code': 200}), 200	
-
-@app.route('/get_workout_schedule', methods=['GET'])
-def get_workout_schedule():
-	df = pd.read_csv("workout_schedule.csv")
-
-	# Merge ABS and CARDIO into the main workout category for each day
-	merged_workout = {}
-
-	for _, row in df.iterrows():
-		day = row['DAY']
-		name = row['NAME']
-
-		if day in merged_workout:
-			if name not in merged_workout[day]:
-				merged_workout[day].append(name)
-		else:
-			merged_workout[day] = [name]
-
-	# Convert merged data to the desired format
-	workout_data = [{"DAY": day, "NAME": " + ".join(names)} for day, names in merged_workout.items()]
-	return jsonify({'workout_schedule': workout_data, 'status_code': 200}), 200
-
-@app.route('/get_workout_details/<day>', methods=['GET'])
-def get_workout_details(day):
-	df = pd.read_csv("./workout_schedule.csv")
-	df1 = df[df.DAY == day.upper()]
-	name = df1['NAME'].unique().tolist()
-	to_send_list = []
-	for _, exer in df1.iterrows():
-		to_send_dict = {
-			'id': exer['ID'],
-			'name': exer['EXERCISE'],
-			'sets': exer['SETS'],
-			'reps': exer['REPS']
+	sending_list = []
+	for key in vals:
+		sending_dict = {
+			"day": key,
+			"exercise": vals[key]['exercise'],
+			"id": vals[key]['id'],
+			"key": vals[key]['exercise_key']
 		}
-		to_send_list.append(to_send_dict)
-	return jsonify({'data': to_send_list, 'status_code': 200, 'day': day, 'name': ' + '.join(name)}), 200
+		sending_list.append(sending_dict)
+
+	# Custom day order
+	day_order = [
+		"monday", "tuesday", "wednesday", "thursday",
+		"friday", "saturday", "sunday"
+	]
+
+	# Sort by custom order
+	sending_list.sort(key=lambda x: day_order.index(x["day"].lower()))
+	print("Sending List: \n", sending_list)
+	return jsonify({'status_code': 200, 'data': sending_list}), 200
+
+
+@app.route('/get_workout_details_v2/<id>', methods=['GET'])
+def get_workout_details_v2(id):
+	print("# ---- get_workout_details_v2 ----")
+	print("Request from client: ", id)
+
+	exercise_data = db.child('dataset').child('exercise').child(id).get().val()
+	print("Raw data from DB:", exercise_data)
+
+	sending_list = []
+	if exercise_data:   # ✅ only loop if data exists
+		for key in exercise_data:
+			print("Key:", key)
+			if 'desc' not in exercise_data[key]:
+				exercise_data[key]['desc'] = ''
+			sending_dict = {
+				'id': key,
+				'details': exercise_data[key]
+			}
+			sending_list.append(sending_dict)
+	else:
+		print("⚠️ No data found for:", id)
+
+	return jsonify({
+		'status': 'Working',
+		'data': sending_list   # will be [] if no exercises (like Sunday)
+	})
+
 
 @app.route('/get_progress', methods=['GET'])
 def get_progress():
@@ -277,6 +360,7 @@ def get_history():
 def get_history_details(id):
 	histDetail = db.child('session').child(id).get()
 	to_send_list = []
+	print(histDetail.val())
 	if histDetail:
 		to_send_list.append(histDetail.val())
 		return jsonify({'data': to_send_list, 'status_code': 200}), 200
@@ -286,6 +370,8 @@ def get_history_details(id):
 def upload_session():
 	print("--- upload session ---")
 	data = request.get_json()
+	print("Data from the client:\n",data)
+	# return jsonify({'status':'still working'}), 200
 	date_str = data['date'] # This goes to session
 	date_obj = datetime.strptime(date_str, "%Y-%m-%d")
 	formatted_date = date_obj.strftime("%b %d, %Y") # This goes to History
@@ -299,6 +385,14 @@ def upload_session():
 			return jsonify({'msg': 'Updated the session key!', 'status_code': 200}), 200
 
 	elif sessTime.lower() == 'morning':
+		exKey = data['day']
+		print("exercise key: ",exKey)
+		nameData = db.child('dataset').child('schedule').get().val()
+		
+		exercise_name = next((day_info['exercise'] for day_info in nameData.values() if day_info['exercise_key'] == exKey),None)
+		print(f"Exercise Name: {exercise_name}| Key: {exKey}")
+		data['day'] = exercise_name
+		# return jsonify({'status':'still working'}), 200
 		sessData = db.child('session').push(data)
 		if sessData:
 			sessionKey = sessData['name']
@@ -319,7 +413,9 @@ def upload_session():
 			}
 			histDataResp = db.child('history').push(histData)
 			if histDataResp:
-				return jsonify({'msg': 'Data added to sesssion.', 'status_code': 200, 'key': histDataResp['name']}), 200
+				cnt = db.child('streak').get().val()
+				print("Streak: ",cnt)
+				return jsonify({'msg': 'Data added to sesssion & streak updated.', 'status_code': 200, 'key': histDataResp['name']}), 200
 			else:
 				return jsonify({'msg': 'Failed to add History data.', 'status_code': 400, 'key': None}), 400
 		else:
@@ -457,14 +553,17 @@ def get_weights():
 
 @app.route('/get_workout_day',methods=['GET'])
 def get_workout_day():
+	print("---- GET WORKOUT DAY ----")
 	date = datetime.today().strftime("%A")
-	df =  pd.read_csv('./datasets/schedule.csv')
-	exercise_name = df[df['day'] == date]['exercise'].values[0]
-	df = pd.read_csv(f'./datasets/{date.lower()}.csv')
-	print(df['exercise'].to_list())
+	sch_df = db.child('dataset').child('schedule').get()
+
+	exercise_name = sch_df.val()[date.lower()]['exercise']
+	exercise_key = sch_df.val()[date.lower()]['exercise_key']
+	exercise_data = db.child('dataset').child('exercise').child(exercise_key).get().val()
+	print("exercise data\n",exercise_data)
 	to_send_dict = {
 		'name':exercise_name,
-		'exercise_list':df['exercise'].to_list()
+		'exercise_list':list(exercise_data.values())[1:]
 	}
 	return jsonify({'status_code':200,'data':to_send_dict}), 200
 
@@ -473,27 +572,177 @@ def update_exercise(id):
 	data = request.get_json()
 	print("----- updating exercise -----")
 	print("Data: ", data)
-	file_name = data['day'].lower()
-	df = pd.read_csv(f'./datasets/{file_name}.csv')
-	print(df)
-	# Update the row where id matches
-	df.loc[df['id'] == data['id'], ['exercise', 'sets', 'reps', 'desc']] = [
-		data['name'],
-		data['sets'],
-		data['reps'],
-		data['desc']
-	]
-	df.to_csv(f'./datasets/{file_name}.csv', index=False)
-	return jsonify({'status': 'Data successfully Updated!', 'status_code': 200}), 200
+	to_update_date = {
+		'desc':data['desc'],
+		'sets':data['sets'],
+		'reps':data['reps']
+	}
+	updated_ref = db.child('dataset').child('exercise').child(data['day']).child(data['id']).update(to_update_date)
 
-"""
+	if updated_ref:
+		print(updated_ref)
+		return jsonify({'status':'Updated the data'}), 200
+	else:
+		return jsonify({'status': 'Updation Failed.'}), 400
+
+
+@app.route('/add_new_exercise/<id>', methods=['POST'])
+def add_new_exercise(id):
+	print("--- Adding new Exercise ---")
+	print("Request ID: ", id)
+	data = request.get_json()
+	print(f"Data from the client:\n{data}")
+	exId = data['id']
+	exData = {k: v for k, v in data.items() if k != "id"}
+	newEx = db.child('dataset').child('exercise').child(id).child(exId).set(exData)
+	if newEx:
+		print(f"Data successfully added! {newEx}")
+		return jsonify({'status': 'Data added succesfully', 'key':exId}), 200
+	else:
+		print(f"Data addition failed!")
+		return jsonify({'status': 'Data addition failed.', 'key':None}), 400
+	
+
+@app.route('/get_streak_counter', methods=['GET'])
+def get_streak_counter():
+	print("--- get streak counter ---")
+	cnt = db.child('streak').get().val()
+	print("Streak: ",cnt, type(cnt))
+	return jsonify({'status': 'Still working', 'counter':str(cnt)}),200
+
+@app.route('/get_profile', methods=['POST'])
+def get_profile():
+	print("--- Getting Profile ---")
+	data = request.get_json()
+	userKey = data['key']
+	print("UserKey from the client: ", userKey)
+	userProfile = db.child('users').child(userKey).get().val()
+	print(userProfile)
+	to_send_dict = {
+		'firstName': userProfile['firstName'],
+		'lastName': userProfile['lastName'],
+		'email': userProfile['email'],
+		'phoneNumber': userProfile['phoneNumber']
+	}
+	
+	return jsonify({'status': 'Getting the profile', 'data':to_send_dict}), 200
+
+@app.route('/get_daily_workouts',methods=['GET'])
+def get_daily_workouts():
+	print("---- get daily workouts ----")
+	
+	try:
+		# Step 1: Batch fetch all required data
+		histData = db.child('history').get()
+		all_sessions = db.child('session').get()
+		schedule_data = db.child('dataset').child('schedule').get()
+		
+		# Step 2: Create lookup dictionaries for O(1) access
+		sessions_dict = {}
+		if all_sessions:
+			for session in all_sessions.each():
+				sessions_dict[session.key()] = session.val()
+		
+		schedule_dict = {}
+		if schedule_data:
+			for day_data in schedule_data.each():
+				schedule_dict[day_data.key()] = day_data.val()['exercise']
+		
+		to_send_list = []
+		processed_session_ids = set()  # Track which sessions we've processed
+		
+		# Step 3: Process history data (existing logic but with lookups)
+		if histData:
+			for hist in histData.each():
+				session_id = hist.val()['sessionId']
+				processed_session_ids.add(session_id)
+				
+				# Use dictionary lookup instead of database call
+				session_data = sessions_dict.get(session_id)
+				if session_data:
+					day_lower = session_data['day'].lower()
+					exercise_name = schedule_dict.get(day_lower, 'Unknown Exercise')
+					
+					to_send_list.append({
+						'histKey': hist.key(),
+						'date': hist.val()['date'],
+						'sessKey': session_id,
+						'sessDay': session_data['day'],
+						'sessName': exercise_name
+					})
+		
+		# Step 4: Process sessions that don't have history records
+		for session_key, session_data in sessions_dict.items():
+			if session_key not in processed_session_ids:
+				day_lower = session_data['day'].lower()
+				exercise_name = schedule_dict.get(day_lower, 'Unknown Exercise')
+				
+				to_send_list.append({
+					'histKey': None,  # No history record exists
+					'date': session_data.get('date', None),  # Use session date if available
+					'sessKey': session_key,
+					'sessDay': session_data['day'],
+					'sessName': exercise_name
+				})
+		
+		return jsonify({'status': 'Got the data.', 'data': to_send_list}), 200
+		
+	except Exception as e:
+		print(f"Error in get_daily_workouts: {str(e)}")
+		return jsonify({'status': 'Failed', 'data': []}), 400
+
+
+@app.route('/get_daily_workout_report', methods=['POST'])
+def get_daily_workout_report():
+	print("--- /get_daily_workout_report ---")
+	data = request.get_json()
+	hKey = data['histKey']
+	sKey = data['sessKey']
+	print(f"history: {hKey} | session: {sKey}")
+	
+	histData = db.child('history').child(hKey).get().val()
+	sessData = db.child('session').child(sKey).get().val()
+	print(f"---- HISTORY ----\n{histData}\n---- SESSION ----\n{sessData}")
+	
+	# Extract meals data from history (excluding sessionId and date)
+	meals_data = {
+		'breakfast': histData.get('breakfast', ''),
+		'lunch': histData.get('lunch', ''),
+		'dinner': histData.get('dinner', ''),
+		'preWorkout': histData.get('preWorkout', ''),
+		'snack': histData.get('snack', ''),
+		'workout': histData.get('workout', ''),
+		'weight': histData.get('weight', ''),
+		'hip': histData.get('hip', ''),
+		'stomach': histData.get('stomach', ''),
+		'thigh': histData.get('thigh', ''),
+		'waist': histData.get('waist', '')
+	}
+	exData = db.child('dataset').child('schedule').child(sessData.get('day', '').lower()).get()
+	# Extract session data (excluding date)
+	session_data = {
+		'day': sessData.get('day', ''),
+		'name': exData.val()['exercise'],
+		'exercise': sessData.get('exercise', {}),
+		'sessionTime': sessData.get('sessionTime', ''),
+		'totalTimeTaken': sessData.get('totalTimeTaken', '')
+	}
+	
+	# Create combined response
+	response_data = {
+		'meals': meals_data,
+		'session': session_data,
+		'date': histData.get('date', '')  # Keep one date reference
+	}
+	
+	return jsonify({'status':'Completed!','data':response_data}), 200
+'''
 TODO: 
-Add the file to github. Vercel will automatically take it.
-Add a single exercise.
-"""
+Checkout the upload session part. The exercise is undefined and the key of the exercise is going in name.
+'''
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8080))
 	# CHANGE THIS TO 0.0.0.0 WHILE PUBLISHING ON HEROKU
 	app.run(host='0.0.0.0', port=port, debug=True)
-	# app.run(host='192.168.68.57', port=port, debug=True)
+	# app.run(host='10.236.36.36', port=port, debug=True)
